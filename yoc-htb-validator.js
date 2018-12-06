6b80debaafb38844cde326ed13e9008ba9a07ae7
Nikolay Sokolov <belnamtar@mail.ru>
2018-12-06 17:27:15 +0300

    Implement YocHtb adapter

--- yoc-htb-validator.js
+++ yoc-htb-validator.js
@@ -29,7 +29,7 @@ function partnerValidator(configs) {
                     '*': {
                         type: 'object',
                         properties: {
-                            placementId: {
+                            adSlotId: {
                                 type: 'string',
                                 minLength: 1
                             }
